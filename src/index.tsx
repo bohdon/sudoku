8d422af573374474e4d1fbcef15d4aa8931922c1
Bohdon Sayre <bohdon@gmail.com>
2024-01-07 23:46:18 -0500

    convert app to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./style.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as Element);
 root.render(
   <StrictMode>
     <App />
